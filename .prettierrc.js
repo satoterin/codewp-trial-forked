9a4ae783b69a1f5fbd358e971be4b5da3a0ff385
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "arrowParens": "always",
+  "bracketSameLine": false,
+  "bracketSpacing": true,
+  "jsxSingleQuote": false,
+  "parser": "typescript",
+  "printWidth": 120,
+  "semi": true,
+  "singleQuote": true,
+  "tabWidth": 4,
+  "trailingComma": "all",
+  "useTabs": true,
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => (
+	<div className="w-full h-full flex flex-col text-white bg-slate-700 justify-center items-center">
+		<h1 className="text-teal-300 font-bold text-2xl">
+			Code WP
+		</h1>
+		<p>Start coding your component here</p>
+	</div>
+);
+export default App;
